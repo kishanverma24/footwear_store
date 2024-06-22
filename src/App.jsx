782154342170652a77547d1f2a7eac726e55cc58
kishanverma24 <kishanverma9151@gmail.com>
2024-06-22 16:50:37 +0530

    cart section's bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ const App = () => {
     <>
       <Router>
         <Routes>
-
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -6,19 +6,28 @@ const ShoppingCart = () => {
       id: 1,
       name: "Dingo Dog Bones",
       description:
-        "The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.",
+        "The best dog bones of all time. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.",
       price: 12.99,
-      quantity: 2,
+      quantity: 1,
       image: "/images/nike7.png",
     },
     {
       id: 2,
-      name: "Nutro™ Adult Lamb and Rice Dog Food",
+      name: "Dingo Dog Bones",
       description:
-        "Who doesn't like lamb and rice? We've all hit the halal cart at 3am while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!",
-      price: 45.99,
+        "The best dog bones of all time. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.",
+      price: 12.99,
       quantity: 1,
       image: "/images/nike8.png",
+    },
+    {
+      id: 3,
+      name: "Dingo Dog Bones",
+      description:
+        "The best dog bones of all time. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.",
+      price: 12.99,
+      quantity: 1,
+      image: "/images/nike9.png",
     },
   ]);
 
@@ -47,6 +56,9 @@ const ShoppingCart = () => {
   const calculateTotal = (subtotal, tax, shipping) => {
     return (parseFloat(subtotal) + parseFloat(tax) + shipping).toFixed(2);
   };
+  const handleAdd = () => {
+    console.log("hello");
+  };
 
   const subtotal = calculateSubtotal();
   const tax = calculateTax(subtotal);
@@ -54,79 +66,91 @@ const ShoppingCart = () => {
   const total = calculateTotal(subtotal, tax, shipping);
 
   return (
-    <div className="shoppingCart">
-      <h1>Shopping Cart</h1>
-      <div className="columnLabels">
-        <label className="productImage">Image</label>
-        <label className="productDetails">Product</label>
-        <label className="productPrice">Price</label>
-        <label className="productQuantity">Quantity</label>
-        <label className="productRemoval">Remove</label>
-        <label className="productLinePrice">Total</label>
-      </div>
+    <>
+      <div className="shoppingCart">
+        <h1>Shopping Cart</h1>
+        <div className="columnLabels">
+          <label className="productImage">Image</label>
+          <label className="productDetails">Product</label>
+          <label className="productPrice">Price</label>
+          <label className="productQuantity">Quantity</label>
+          <label className="productRemoval">Remove</label>
+          <label className="productAdd">Add to checkout</label>
+        </div>
 
-      {products.map((product) => (
-        <div className="product" key={product.id}>
-          <div className="productImage">
-            <img src={product.image} alt="Product" />
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <div className="productImage">
+              <img src={product.image} alt="Product" />
+            </div>
+            <div className="_productDetails">
+              <div className="productTitle">{product.name}</div>
+              <p className="productDescription">{product.description}</p>
+            </div>
+            <div className="productPrice">${product.price.toFixed(2)}</div>
+            <div className="productQuantity">
+              <input
+                type="number"
+                value={product.quantity}
+                min="1"
+                onChange={(e) =>
+                  handleQuantityChange(product.id, parseInt(e.target.value))
+                }
+              />
+              <p className="multipleprice" style={{ marginTop: "5px" }}>
+                {" "}
+                ${(product.price * product.quantity).toFixed(2)}
+              </p>
+            </div>
+            <div className="productRemoval">
+              <button
+                className="removeProduct"
+                onClick={() => handleRemove(product.id)}
+              >
+                Remove
+              </button>
+            </div>
+            <div className="productAdd">
+              <button
+                className="addProduct"
+                onClick={() => handleAdd(product.id)}
+              >
+                Add to checkout
+              </button>
+            </div>
           </div>
-          <div className="productDetails">
-            <div className="productTitle">{product.name}</div>
-            <p className="productDescription">{product.description}</p>
-          </div>
-          <div className="productPrice">${product.price.toFixed(2)}</div>
-          <div className="productQuantity">
-            <input
-              type="number"
-              value={product.quantity}
-              min="1"
-              onChange={(e) =>
-                handleQuantityChange(product.id, parseInt(e.target.value))
-              }
-            />
-          </div>
-          <div className="productRemoval">
-            <button
-              className="removeProduct"
-              onClick={() => handleRemove(product.id)}
-            >
-              Remove
-            </button>
-          </div>
-          <div className="productLinePrice">
-            ${(product.price * product.quantity).toFixed(2)}
-          </div>
-        </div>
-      ))}
+        ))}
 
-      <div className=".totals">
-        <div className="totalsItem">
-          <label>Subtotal</label>
-          <div className="totalsValue" id="cart-subtotal">
-            ${subtotal}
+        <div className=".totals">
+          <div className="totalsItem">
+            <label>Subtotal</label>
+            <div className="totalsValue" id="cart-subtotal">
+              ${subtotal}
+            </div>
           </div>
-        </div>
-        <div className="totalsItem">
-          <label>Tax (5%)</label>
-          <div className="totalsValue" id="cart-tax">
-            ${tax}
+          <div className="totalsItem">
+            <label>Tax (5%)</label>
+            <div className="totalsValue" id="cart-tax">
+              ${tax}
+            </div>
           </div>
-        </div>
-        <div className="totalsItem">
-          <label>Shipping</label>
-          <div className=".totalsValue" id="cart-shipping">
-            ${shipping.toFixed(2)}
+          <div className="totalsItem">
+            <label>Shipping</label>
+            <div className=".totalsValue" id="cart-shipping">
+              ${shipping.toFixed(2)}
+            </div>
           </div>
-        </div>
-        <div className={`${"totalsItem"} ${"totalsItemTotal"}`}>
-          <label>Grand Total</label>
-          <div className="totalsValue" id="cart-total">
-            ${total}
+          <div className={`${"totalsItem"} ${"totalsItemTotal"}`}>
+            <label>Grand Total</label>
+            <div className="totalsValue" id="cart-total">
+              ${total}
+            </div>
           </div>
+
+          <button className="checkoutButton">Checkout</button>
         </div>
-        <button>Checkout</button>
       </div>
-    </div>
+    </>
   );
 };
 
