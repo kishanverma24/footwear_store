250f67fcf41642e3964114a357dbed3d1aceec40
kishanverma24 <kishanverma9151@gmail.com>
2024-06-23 21:01:21 +0530

    added the functionality to save the cart items to localstorage

--- src/context/Cart.jsx
+++ src/context/Cart.jsx
@@ -1,7 +1,24 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+const getCartItemsFromLocalStorage = () => {
+  const storedCartItems = localStorage.getItem("cartItems");
+  return storedCartItems ? JSON.parse(storedCartItems) : [];
+};
+
+const setCartItemsToLocalStorage = (cartItems) => {
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+};
+
 export const CartContext = createContext(null);
+
 export const CartProvider = (props) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() =>
+    getCartItemsFromLocalStorage()
+  );
+
+  useEffect(() => {
+    setCartItemsToLocalStorage(cartItems);
+  }, [cartItems]);
+
   return (
     <CartContext.Provider value={{ cartItems, setCartItems }}>
       {props.children}
