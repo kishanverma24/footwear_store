dc5e6d99e4c66dc2d40ce57195caa1077b721d75
kishanverma24 <kishanverma9151@gmail.com>
2024-06-25 22:25:40 +0530

    comment section fixed

--- src/pages/productDetails/ProductDetails.jsx
+++ src/pages/productDetails/ProductDetails.jsx
@@ -21,16 +21,13 @@ const ProductDetails = () => {
   }, [productid]);
 
   const addCartItem = () => {
-    // Check if the product is already in cartItems
     const isProductInCart = cartItems.some(
       (item) => item.productId === product.productId
     );
 
     if (!isProductInCart) {
-      // If product is not in cart, add it to cartItems
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
     } else {
-      // Optionally, you can provide feedback to the user that the product is already in cart
       alert("This product is already in your cart.");
     }
   };
@@ -74,7 +71,43 @@ const ProductDetails = () => {
             <p>Description: {product.description}</p>
 
             <div className="comments-section">
-              {/* Your comments section JSX */}
+              <div className="comments-header">
+                <h2>Likes:355</h2>
+                <h2>Comments (3)</h2>
+              </div>
+              <div className="comment-input">
+                <input type="text" placeholder="Write your comment here..." />
+                <button type="button">Add</button>
+              </div>
+              <div className="comment">
+                <div className="comment-content">
+                  <h4>Brigita</h4>
+                  <p>
+                    I completely agree with love. Nowadays people are proud of
+                    being busy and you are labeled as lazy or useless when you
+                    are not ‘doing’. But it is this ‘not doing’ that
+                  </p>
+                </div>
+              </div>
+              <div className="comment">
+                <div className="comment-content">
+                  <h4>Rens Klein</h4>
+                  <p>
+                    I really would like to suggest the following book; The art
+                    of stillness.
+                  </p>
+                </div>
+              </div>
+              <div className="comment">
+                <div className="comment-content">
+                  <h4>Josh Spilker</h4>
+                  <p>
+                    I completely agree with loud. Nowadays people are proud of
+                    being busy and you are labeled as lazy or useless when you
+                    are not ‘doing’. But it is this ‘not doing’ that
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
